c402b76a8c24df542faa1e68ce08630312763fb8
unknown <doanncd00530@fpt.edu.vn>
2018-10-25 19:57:27 +0700

    Bai Thi

---
+++ BaiThiUWP/Entity/SaveFile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiThiUWP.Entity
+{
+    class SaveFile
+    {
+        private String _fileName;
+        private String _content;
+
+        public string fileName { get => _fileName; set => _fileName = value; }
+        public string content { get => _content; set => _content = value; }
+    }
+}
---
+++ BaiThiUWP/Views/AddFile.xaml.cs
@@ -0,0 +1,54 @@
+﻿using BaiThiUWP.Entity;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace BaiThiUWP.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class AddFile : Page
+    {
+        private SaveFile saveFile;
+       
+        public AddFile()
+        {
+            this.InitializeComponent();
+            this.saveFile = new SaveFile();
+            
+        }
+
+        private async void btnSaveFile(object sender, RoutedEventArgs e)
+        {
+            this.saveFile.fileName = this.NameFile.Text;
+            this.saveFile.content = this.Content.Text;
+
+            Windows.Storage.StorageFolder storageFolder =
+            Windows.Storage.ApplicationData.Current.LocalFolder;
+            Windows.Storage.StorageFile sampleFile =
+                await storageFolder.CreateFileAsync("myfile.txt",
+                    Windows.Storage.CreationCollisionOption.ReplaceExisting);
+            var savePicker = new Windows.Storage.Pickers.FileSavePicker();
+            savePicker.SuggestedStartLocation =
+                Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary;
+            // Dropdown of file types the user can save the file as
+            savePicker.FileTypeChoices.Add("Plain Text", new List<string>() { ".txt" });
+            // Default file name if the user does not type one in or select a file to replace
+            savePicker.SuggestedFileName = "New Document";
+        }
+    }
+}
